5de255a8f48a0033b62e6077a6240f8ec402916b
walacetalesfs <walacetales@hotmail.com>
2020-01-07 20:44:07 -0300

    modelo de usuário adicionado

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs')
+
+const mongoose = require('../config/database');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
