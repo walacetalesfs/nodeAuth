df9d0d9a66ecb2bd1c83885a62e8fecba6c9c5b3
walacetalesfs <walacetales@hotmail.com>
2020-01-09 11:22:23 -0300

    adição do projectController

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./app/controllers/authController')(app);
+require('./app/controllers/projectController')(app);
 
 app.listen(constants.PORT, () => { console.log(`servidor rodando em ${constants.URL}`) });
